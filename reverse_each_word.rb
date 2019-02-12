6640f8d6fbd7158ba99cdb7a6cd683aa8ae7ffaf
Brian Simonton <bsimonton78@gmail.com>
2019-02-12 01:38:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -16,18 +16,3 @@ end
 
 
 
-def reverse_each_word(string)
-  
-  array = string.split(" ")
-  
-  rev_array = []
-  
-  array.collect do |string|
-    
-   string.reverse
-    
-  end
-  
-  rev_array.join(" ")
-  
-end
