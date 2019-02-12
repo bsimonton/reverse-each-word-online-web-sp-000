b43b566435a88d180761dcbae15e590a858a4b51
Brian Simonton <bsimonton78@gmail.com>
2019-02-12 01:43:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,15 +2,17 @@ def reverse_each_word(string)
   
   array = string.split(" ")
   
+  rev_array = []
   
   array.collect do |string|
     
-  string.reverse
+    rev_array << string.reverse
     
   end
-
   
+  rev_array.join(" ")
 end
+
 
 
 
